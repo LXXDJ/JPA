a4f04a3084ee4e6dd9208f24eabd8b13a9caacf1
LXXDJ <joker96.dj@gmail.com>
2024-05-07 11:25:11 +0900

    first commit

---
+++ chap01-persistence-context/src/test/java/com/ohgiraffers/section01/entitymanager/EntityManagerGeneratorTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section01.entitymanager;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class EntityManagerGeneratorTests {
+
+    @Test
+    @DisplayName("엔터티 매니저 팩토리 생성확인")
+    void testGenerateEntityManagerFactory(){
+        
+    }
+}
